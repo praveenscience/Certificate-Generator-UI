1d247ce77844f9bf788799cfe9c911df4c3db38a
Kashish Ahuja <kashish.ahuja-IT-2K19-24@iips.edu.in>
2021-10-16 19:47:30 +0530

    Update script.js

--- kashishahuja2002/assets/js/script.js
+++ kashishahuja2002/assets/js/script.js
@@ -1,10 +1,10 @@
 $(function () {
-    $("#claim").submit(function (e) {
-        e.preventDefault();
-    });
+  $("#claim").submit(function (e) {
+    e.preventDefault();
+  });
 
-    $("#verify").submit(function (e) {
-        e.preventDefault();
-    });
+  $("#verify").submit(function (e) {
+    e.preventDefault();
+  });
 });
   
